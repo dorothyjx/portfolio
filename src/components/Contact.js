aeda3d604eb3e9194db95d24ba5cf5c0dce9858a
Dorothy Ji <dorothyjx@gmail.com>
2022-11-23 19:16:21 -0800

    import @emailjs/browser for email sending

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,36 @@
-import React, {useState} from "react"
+import React, {useRef} from "react"
+import emailjs from '@emailjs/browser';
+
 import "./ContactStyle.css"
 
 
 const Contact = () => {
 
-	const handleSubmit = (e) => {
-		e.preventDefault()
-		let name = e.target.name.value;
-		let email = e.target.email.value;
-		let msg = e.target.msg.value;
-	}
+	const form = useRef()
+
+	const sendEmail = (e) => {
+		e.preventDefault(); 
+	
+		emailjs.sendForm('service_91kgpwj', 'template_o0zmx89', form.current, '_ffIAFGxNkAaIV82T')
+		  	.then((result) => {
+			  	console.log(result.text);
+		  	}, (error) => {
+			  	console.log(error.text);
+		});
+		e.target.reset()
+	};
 
 	return <>
 		<div id="contact">
 			<h1>Contact</h1>
 			<small>(Only for display)</small>
-			<form className="contact" onClick={handleSubmit}>
+			<form className="contact" ref={form} onSubmit={sendEmail}>
 				<input type="text" name="name" placeholder="Name" required/>
     			<label className="label" htmlFor="name">Name</label>
     			<input type="email" name="email" placeholder="Email" required/>
     			<label className="label" htmlFor="email">Email</label>
-    			<textarea name="msg" placeholder="Message"></textarea>
-    			<label className="label-textarea" htmlFor="msg">Message</label>
+    			<textarea name="message" placeholder="Message"></textarea>
+    			<label className="label-textarea" htmlFor="message">Message</label>
     			<input type="submit" name="send" value="Send"/>
 			</form>
 		</div>
